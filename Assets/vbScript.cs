dfeada9b20eba2934d4668104d47bff42990477f
syikin <syikinalwi@gmail.com>
2018-05-07 01:38:54 +0800

    redo of previos ar

---
+++ Assets/vbScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+	private GameObject vbButtonObject;
+	public GameObject vbmoon;
+
+
+	void Start () {
+		vbButtonObject = GameObject.Find("vbmoon");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		vbmoon = GameObject.Find("vbmoon");
+		
+	}
+	
+	// Update is called once per frame
+	public void OnButtonPressed (VirtualButtonBehaviour vb) 
+	{
+		Debug.Log("Play");
+		vbmoon.GetComponent<AudioSource>().Play();
+		
+	}
+	
+	public void OnButtonReleased (VirtualButtonBehaviour vb)
+	{
+		Debug.Log("Stop");
+		vbmoon.GetComponent<AudioSource>().Stop();
+	}
+}
---
+++ Assets/vbScript1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript1 : MonoBehaviour, IVirtualButtonEventHandler
+{
+	private GameObject vbButtonObject;
+	public GameObject vbmush;
+
+
+	void Start () {
+		vbButtonObject = GameObject.Find("vbmush");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		vbmush = GameObject.Find("vbmush");
+		
+	}
+	
+	// Update is called once per frame
+	public void OnButtonPressed (VirtualButtonBehaviour vb) 
+	{
+		Debug.Log("Play");
+		vbmush.GetComponent<AudioSource>().Play();
+		
+	}
+	
+	public void OnButtonReleased (VirtualButtonBehaviour vb)
+	{
+		Debug.Log("Stop");
+		vbmush.GetComponent<AudioSource>().Stop();
+	}
+}
